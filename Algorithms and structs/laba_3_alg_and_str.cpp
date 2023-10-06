156374818a5d8b3ae7c41792edfd07d304da1091
Fallmore <135610092+Fallmore@users.noreply.github.com>
2023-10-06 15:36:24 +0400

    Add files via upload

---
+++ Algorithms and structs/laba_3_alg_and_str.cpp
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
+#include <random>
+#include <windows.h>
+
+using namespace std;
+
+int Check()
+{
+	int in; rewind(stdin);
+	while (scanf_s("%d", &in) != 1)
+		rewind(stdin), printf_s("\nНеверные данные, введите повторно: ");
+	return in;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(0, "");
+	stack<char> st; queue<char> qu; string pal; size_t cnt = 0;
+
+	cout << "Самодуров ДИНРБ-21/2\nВариант 7\n\n\n";
+	cout << "Для выхода напишите \"1488\"\n";
+
+	do
+	{
+		cout << "\nВведите слово или число для проверки его на палиндром: "; cin >> pal;
+		if (pal != "1488")
+		{
+			cnt = size(pal);
+
+			for (auto simv : pal) st.push(simv), qu.push(simv);
+
+			for (size_t i = 0; i < cnt; ++i)
+			{
+				if (st.top() == qu.front()) st.pop(), qu.pop();
+				else
+				{
+					cout << "\nНе палиндром\n"; break;
+				}
+			}
+			if (st.empty()) cout << "\nПалиндром\n";
+			else 
+			{
+				cout << "Не хватает символа: " << st.top() << " или " << qu.front() << "\n";
+				while (!st.empty()) st.pop(), qu.pop();
+			}
+
+			pal = "";
+		}
+	} while (pal != "1488");
+}
+
+//class Plane
+//{
+//private:
+//	int index;
+//	bool isOnLand, isBoarding, isTakeoff;
+//public:
+//	Plane() : index(0), isOnLand(0), isBoarding(0), isTakeoff(0) {};
+//		
+//};
+//
+//class Runway
+//{
+//private:
+//	int allPlaneCount, currentPlaneCount, landingCount, takeoffRequestCount;
+//	void CountingPlanes()
+//	{
+//		int oldCount = currentPlaneCount;
+//		currentPlaneCount = landingCount - takeoffRequestCount;
+//		if (currentPlaneCount - oldCount > 0) allPlaneCount += currentPlaneCount;
+//	}
+//public:
+//	Runway() : allPlaneCount(0), currentPlaneCount(0), landingCount(0), takeoffRequestCount(0) {};
+//	Runway(int land, int request) : landingCount(land), takeoffRequestCount(request) { allPlaneCount = currentPlaneCount = land; }
+//
+//	void Set(const int plane)
+//	{
+//		landingCount = plane;
+//		CountingPlanes();
+//	}
+//	void Set(const int plane, const int req)
+//	{
+//		landingCount = plane;
+//		takeoffRequestCount = req;
+//		CountingPlanes();
+//	}
+//
+//};
+
+int Request(int mean)
+{
+	random_device rd; //генератор случайных чисел. 
+	mt19937 gen(rd()); //генератор псевдослучайных чисел, который инициализируется случайным числом rd().
+	poisson_distribution<>distr(mean); //создание объекта для генерации случайных чисел с распределением Пуассона при заданном среднем (ожидаемом) значении mean.
+	return distr(gen);//генерирование случайного числа с распределением Пуассона с заданным параметром mean.
+}
+
+void Start(int& meanB, int& meanT, int& maxBoarding, int& maxTakeoff, int& time)
+{
+	cout << "Введи максимальное количество запросов на\nПосадку: "; maxBoarding = Check();
+	cout << "Взлёт: "; maxTakeoff = Check();
+	cout << "Введи время для моделирования запросов (значение, как час реального времени): "; time = Check();
+	cout << "Введи предполагаемое (ожидаемое) число самолётов\nПрилетающих: "; meanB = Check();
+	cout << "Взлетающих: "; meanT = Check();
+}
+
+//int main()
+//{
+//	setlocale(LC_ALL, "RUS");
+//	cout << "Самодуров ДИНРБ-21/2\nВариант 7\n\n\n";
+//
+//	int meanB, meanT, mB, mT, maxBoarding, maxTakeoff, time,
+//		landCount = 0, takeoffCount = 0;
+//	queue<int> qB, qT;
+//	Start(meanB, meanT, maxBoarding, maxTakeoff, time);
+//
+//	int boardCountRequests = 0, takeoffCountRequests = 0,
+//		acceptedBoardRequest = 0, acceptedTakeoffRequest = 0,
+//		allBoardCount = 0, allTakeoffCount = 0,
+//		rejectedBoardRequest = 0, rejectedTakeoffRequest = 0,
+//		allTimeWaiting = 0, downtime = 0;
+//
+//	for (int i = 0; i < time; ++i)
+//	{
+//		mB = Request(meanB), mT = Request(meanT);
+//
+//		for (int i = 0; i < mB; ++i, qB.push(1), ++boardCountRequests);
+//		for (int i = 0; i < mT; ++i, qT.push(1), ++takeoffCountRequests);
+//
+//		if (qB.empty() && qT.empty()) ++downtime;
+//
+//		if (!qB.empty())
+//			for (int i = landCount; i < maxBoarding && !qB.empty(); ++i, ++landCount, ++acceptedBoardRequest, ++allBoardCount, qB.pop());
+//		if (!qT.empty())
+//			for (int i = takeoffCount; i < maxTakeoff && !qT.empty(); ++i, ++takeoffCount, --landCount, ++acceptedTakeoffRequest, ++allTakeoffCount, qT.pop());
+//
+//		if (!qB.empty())
+//		{
+//			cout << endl << qB.size() << " отправили на другой аэропорт\n";
+//			while (!qB.empty()) qB.pop(), ++rejectedBoardRequest;
+//		}
+//		if (!qT.empty())
+//		{
+//			cout << endl << qT.size() << " задержали взлёт\n";
+//			while (!qT.empty()) qT.pop(), ++rejectedTakeoffRequest;
+//			takeoffCount = 0;
+//			allTimeWaiting += rejectedTakeoffRequest;
+//		}
+//	}
+//
+//	cout << "\n\nОбщее количество запросов: " << boardCountRequests + takeoffCountRequests;
+//	cout << "\n\nКоличество запросов на\nПосадку: " << boardCountRequests << "\nВзлёт: " << takeoffCountRequests;
+//	cout << "\n\nКоличество принятых запросов на\nПосадку: " << acceptedBoardRequest << "\nВзлёт: " << acceptedTakeoffRequest;
+//	cout << "\n\nКоличество\nПриземлившихся: " << allBoardCount << "\nВзлетевших: " << allTakeoffCount;
+//	cout << "\n\nКоличество отказов на\nПосадку: " << rejectedBoardRequest << "\nВзлёт: " << rejectedTakeoffRequest;
+//	cout << "\n\nОбщее время ожидания самолётов: " << allTimeWaiting;
+//	cout << "\n\nОбщее время простоя: " << (time/100)*downtime << "%";
+//	cout << "\n\nСреднее количество запросов в час на\nПосадку: " << boardCountRequests / time << "\nВзлёт: " << takeoffCountRequests / time << endl;
+//
+//	return 0;
+//}
---
+++ Algorithms and structs/laba_4_alg_and_str.cpp
@@ -0,0 +1,190 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <conio.h>
+
+using namespace std;
+
+template<typename T>
+struct Node
+{
+	T term; string explain;
+	Node<T>* Prev;
+	Node<T>* Next;
+	Node(const T& t = T(), const string& e = string(), Node<T>* p = nullptr, Node<T>* n = nullptr) :
+		term(t), explain(e), Prev(p), Next(n) {}
+
+};
+
+template<typename T>
+class Helper
+{
+	Node<T>* Head;
+	Node<T>* Tail;
+	int size;
+
+public:
+
+	Helper() : Head(NULL), Tail(NULL), size(0) {};
+	void Add(T wrd, string help);		//Метод добавления термина и его определения
+	void Show(int size);				//Метод отображения термина и его определения
+	void Show();						//Метод отображения терминов и их определений
+	void Terms();						//Метод отображения имеющихся терминов
+
+	int Count()											 //Функция возвращающая число элементов в списке
+	{
+		return size; //Возвращаем число элементов списка
+	}
+
+	~Helper()
+	{
+
+		while (size != 0)
+		{
+			Node<T>* temp = Head->Next;
+
+			delete Head;
+
+			Head = temp;
+			--size;
+		}
+	}
+
+};
+
+
+template<class T>void Helper<T>::Add(T trm, string expl)
+{
+	++size;
+	Node<T>* temp = new Node<T>;
+
+	temp->term = trm;
+	temp->explain = expl;
+
+	if (Head != NULL)
+	{
+		Tail->Next = temp;
+		temp->Prev = Tail;
+		Tail = temp;
+	}
+	else Head = Tail = temp;
+}
+
+template<class T>void Helper<T>::Terms()
+{
+	Node<T>* tempHead = new Node<T>; //Создаём...
+	tempHead = Head; //...копию головы
+	int temp = Count(); //Временная переменная равная числу элементов в списке
+
+	if (Count() == 0) cout << "\nТермины отсутствуют";
+
+	while (temp != 0) //Пока не выполнен признак прохода по всему списку
+	{
+		cout << endl << Count() - temp + 1 << ". " << tempHead->term << "\n"; //Очередной элемент списка на экран 
+		tempHead = tempHead->Next; //Указываем, что нужен следующий элемент
+		--temp; //Один элемент считан, значит осталось на один меньше 
+	}
+}
+
+template<class T>void Helper<T>::Show(int index)
+{
+	Node<T>* tempHead = new Node<T>; //Создаём...
+	tempHead = Head; //...копию головы
+	int temp = 0;
+	if (index <= Count() && index >= 0 && Count() != 0)
+	{
+		temp = index; //Временная переменная равная номеру элемента в списке
+
+		while (temp != 0) //Пока не выполнен признак прохода по всему списку
+		{
+			tempHead = tempHead->Next; //Указываем, что нужен следующий элемент
+			--temp; //Один элемент считан, значит осталось на один меньше 
+		}
+		cout << endl << tempHead->term << " - " << tempHead->explain << "\n\n"; //Очередной элемент списка на экран 
+	}
+	else cout << "\nВыход за пределы списка терминов\n\n";
+}
+template<class T>void Helper<T>::Show()
+{
+	Node<T>* tempHead = new Node<T>; //Создаём...
+	tempHead = Head; //...копию головы
+	int temp = Count(); //Временная переменная равная числу элементов в списке
+
+	if (Count() == 0) cout << "\nТермины отсутствуют";
+
+	while (temp != 0) //Пока не выполнен признак прохода по всему списку
+	{
+		cout << endl << Count() - temp + 1 << ". " << tempHead->term << " - " << tempHead->explain << "\n\n"; //Очередной элемент списка на экран 
+		tempHead = tempHead->Next; //Указываем, что нужен следующий элемент
+		--temp; //Один элемент считан, значит осталось на один меньше 
+	}
+}
+
+void Menu(Helper<string>& dict)
+{
+	int choice = -1, i = 5;
+	string term;
+	string explain;
+	cout << endl << R"(Выбери действие
+1. Добавить термин с определением
+2. Показать термин с определением
+3. Показать все термины
+4. Показать все термины с определениями
+
+5. Выход
+
+Ввод: )";
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		char ch;
+
+		cout << "\n\nВведите термин: ";
+		cin >> term;
+		cout << "\nВведите определение (Для выхода нажмите \"Ctrl+[\"): ";
+		cin.ignore();
+		while (i != 0)
+		{
+			ch = cin.get();
+			if (ch == 27) break;
+			explain += ch;
+			if (ch == '\n') --i;
+		}
+		dict.Add(term, explain);
+		break;
+	case 2:
+		int index;
+		cout << "\nВведи номер термина: ";
+		cin >> index;
+		if (index > 0) --index;
+		dict.Show(index);
+		break;
+	case 3:
+		dict.Terms();
+		break;
+	case 4:
+		dict.Show();
+		break;
+	case 5:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+
+	Menu(dict);
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(0, "");
+	cout << "Самодуров ДИНРБ-21/2\nВариант 6\n\n\n";
+
+	Helper<string> dict;
+	Menu(dict);
+}
+
