8388dfe065903fc991d6aa4905c5f4ac31be89c2
Fallmore <135610092+Fallmore@users.noreply.github.com>
2023-10-14 16:33:40 +0400

    Недоделка

---
+++ Algorithms and structs/Laba5/Laba_5_alg_and_str.cpp
@@ -0,0 +1,225 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <math.h>
+#include "binarytrees.h"
+
+using namespace std;
+
+int Check()
+{
+	int a;
+	while (!(cin >> a))
+		cout << "\nНеверный Ввод!\n\nВведите снова: ", cin.clear(), cin.ignore();
+	return a;
+}
+void Check(int& choice, int NumberOfChoice)
+{
+	cin.clear();
+	int rep = 0;
+	do
+	{
+		if (!(cin >> choice))
+		{
+			if (rep < 1) cout << "\nДействие отсутствует" << endl << "Введите повторно: ";
+			rep = 1;
+		}
+		else if (choice > NumberOfChoice || choice < 1)
+		{
+			if (rep < 1) cout << "\nДействие отсутствует" << endl << "Введите повторно: ";
+			rep = 1;
+		}
+	} while (getchar() != '\n');
+	if (rep != 0) Check(choice, NumberOfChoice);
+}
+
+//								64
+//				32								96
+//		16				48				80				112
+//	8		24		40		56		72		88		104		120
+//4	  10  20  28  36  44  52  60  68  76 82	  92 100  108 116 124
+//																														64
+//													32													Ты хочешь что-нибудь сделать?											96
+//					16					У тебя есть долги/домашка?						48																	80			Ты хочешь отдохнуть?						112
+//			Делай долги/домашку						40						У тебя есть дела по дому?				56									Отдыхай			104						Ты хочешь что-то сделать, но без усилий?		120
+//												Делай дела					52							У тебя хоть есть силы на дела?				60			Попроси помощи у Славы										116				Ты пиццу хочешь?				124
+//									50					У тебя сейчас есть много свободного времени?			54						58	И долго это у тебя?		62												Иди закажи/поешь					122			А просто есть хочешь?					126
+//					Помоги кому-нибудь или саморазвивайся											Посмотри полезное видео		Обратись за помощью				Отдохни																	Иди поешь что-нибудь и вернись				Тогда просто подкрепись, а затем возвращайся сюда
+
+struct Desire
+{
+	int order, level;
+	string text;
+	Desire(int o = 0, int l = -1, string t = "") : order(o), level(l), text(t) {};
+	bool operator<(Desire rd)
+	{
+		return (order < rd.order);
+	}
+	bool operator<(int rd)
+	{
+		return (order < rd);
+	}
+	bool operator>(Desire rd)
+	{
+		return (order > rd.order);
+	}
+	bool operator>(int rd)
+	{
+		return (order > rd);
+	}
+	bool operator==(int rd)
+	{
+		return (order == order);
+	}
+	bool operator==(Desire rd)
+	{
+		return (order == rd.order);
+	}
+};
+
+ostream& operator<<(ostream& t, Desire& d)
+{
+	return (t << d.text);
+}
+
+
+void Will(BinaryTree<Desire>& desireTree)
+{
+	cout << "ДЕРЕВО РЕШЕНИЙ\n------------------------------------------------------\n";
+	int rootOrd = 64, ord = 64, lvl = 0, ch = 0;
+	Desire temp = Desire(ord, lvl, "");
+
+	do
+	{
+		if (ch == 1)
+			ord -= rootOrd / static_cast<int>(pow(2, lvl));
+		else if (ch == 2) ord += rootOrd / static_cast<int>(pow(2, lvl));
+		ch = 0;
+
+		temp = Desire(ord, lvl, "");
+
+		if (desireTree.CountLeefs(temp) != 1)
+		{
+			cout << "\n" << *(desireTree.retriveItem(temp))
+				<< R"(
+1. Да
+2. Нет
+
+Ввод: )";
+			while (ch > 2 || ch < 1) ch = Check();
+
+			++lvl;
+		}
+		else cout << "\n-----------------------------\n" << *(desireTree.retriveItem(temp)) << "\n-----------------------------";
+
+	} while (desireTree.CountLeefs(temp) != 1);
+}
+
+void DesireTree()
+{
+	int ch = 0;
+	BinaryTree<Desire> desireTree;
+	Desire desires[21];
+	desires[0] = Desire(64, 0, "Ты хочешь что-нибудь сделать?"),
+		//Левая ветка от главного вопроса
+		desires[1] = Desire(32, 1, "У тебя есть долги/домашка?"),
+		desires[2] = Desire(16, 2, "Делай долги/домашку"),
+		desires[3] = Desire(48, 2, "У тебя есть дела по дому?"),
+		desires[4] = Desire(40, 3, "Делай дела"),
+		desires[5] = Desire(56, 3, "У тебя хоть есть силы на дела?"),
+		desires[6] = Desire(52, 4, "У тебя сейчас есть много свободного времени?"),
+		desires[7] = Desire(60, 4, "И долго это у тебя?"),
+		desires[8] = Desire(50, 5, "Помоги кому-нибудь или саморазвивайся"),
+		desires[9] = Desire(54, 5, "Посмотри полезное видео"),
+		desires[10] = Desire(58, 5, "Обратись за помощью"),
+		desires[11] = Desire(62, 5, "Отдохни"),
+		//Правая ветка от главного вопроса
+		desires[12] = Desire(96, 1, "Ты хочешь отдохнуть?"),
+		desires[13] = Desire(80, 2, "Отдыхай"),
+		desires[14] = Desire(112, 2, "Ты хочешь что-то сделать, но без усилий?"),
+		desires[15] = Desire(104, 3, "Попроси помощи у Славы"),
+		desires[16] = Desire(120, 3, "Ты пиццу хочешь?"),
+		desires[17] = Desire(116, 4, "Иди закажи/поешь"),
+		desires[18] = Desire(124, 4, "А просто есть хочешь?"),
+		desires[19] = Desire(122, 5, "Иди поешь что-нибудь и вернись"),
+		desires[20] = Desire(126, 5, "Тогда просто подкрепись, а затем возвращайся сюда");
+
+	for (auto& des : desires)
+	{
+		desireTree.insertItem(des);
+	}
+
+	/*do
+	{
+		ch = 0;
+		Will(desireTree);
+
+		cout << R"(
+
+
+Повторить?
+
+1. Да
+2. Нет
+
+Ввод: )"; while (ch < 1 || ch > 2) Check(ch, 2);
+		if (ch == 1) system("cls");
+	} while (ch != 2);
+
+	cout << endl;*/
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "RUS");
+	cout << "Самодуров ДИНРБ-21/2\n\n\n";
+	DesireTree();
+	return 0;
+}
+
+//temp = tree, temproot = root;
+//if (tree->left == nullptr && tree->right != nullptr)	// Один ребёнок
+//{
+//	temp = tree->right;
+//	tree->info = temp->info, tree->left = temp->left;
+//	temp->right == nullptr
+//		? tree->right = nullptr
+//		: tree->right = temp->right;
+//	delete temp, --count;
+//	if (count == 0) tree = nullptr;
+//}
+//else if (tree->right == nullptr && tree->left != nullptr) // Один ребёнок
+//{
+//	temp = tree->left;
+//	tree->info = temp->info, tree->right = temp->right;
+//	temp->left == nullptr
+//		? tree->left = nullptr
+//		: tree->left = temp->left;
+//	delete temp, --count;
+//	if (count == 0) tree = nullptr;
+//
+//}
+//else if (tree->right == nullptr && tree->left == nullptr)
+//{
+//	temproot = GetSuccessor(temp, (*this).root);
+//	if (temproot->right == temp) temproot->right = nullptr;
+//	else temproot->left = nullptr;
+//	delete temp, --count;
+//	if (count == 0) tree = nullptr;
+//}
+//else // Ровно два дочерних узла
+//{
+//	Node<T>* pred = GetPredecessor(tree, temproot);	// Находит «предыдущий» элемент
+//	int leefs = inorderCountLeefs(pred);
+//	tree->info = pred->info;					// Записывает данные в «удаляемый» узел
+//	if (leefs == 1)
+//	{
+//		if (temproot->right == pred) temproot->right = nullptr;
+//		else temproot->left = nullptr;
+//		delete pred, --count;
+//		if (count == 0) tree = nullptr;
+//	}
+//	else deleteNode(pred, temproot);						//Удаляет «предыдущий» элемент
+//}
---
+++ Algorithms and structs/Laba5/binarytrees.h
@@ -0,0 +1,288 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template<class T> struct Node
+{
+
+	T info;// Информационная часть.
+	Node<T>* left, * right;// Указатели на дочерние узлы.
+	// Конструктор по умолчанию.
+	Node() { left = right = nullptr; }
+	// Конструктор с параметрами.
+	Node(const T& d, Node<T>* l = nullptr, Node<T>* r = nullptr) :
+		info(d), left(l), right(r) {};
+};
+
+template<class T> class BinaryTree
+{
+protected:
+	Node<T>* root;	// Указатель на корень дерева. 
+	int count;		// Счётчик элементов
+	//Node<T>* copyTree(BinaryTree<T>* otherTree) const; // Метод, создающий копию дерева otherTree. 
+
+	Node<T>* searchLeef(Node<T>* tree, T& data) const;	// Ищет и возвращает лист с данными.
+	T* searchInfoLeef(Node<T>* tree, T& data) const;	// Ищет и возвращает данные. (хз зачем так)
+	Node<T>* retriveLeef(T& item) const					// Возвращает указатель на объект, если найден
+	{
+		return searchLeef(root, item);
+	}
+
+	void inorder(Node<T>* tree) const;			// Симметричный обход дерева 
+	int inorderCountLeefs(Node<T>* tree) const; // Симметричный обход дерева с подсчётом его детей
+	//void preorder(Node<T>* tree) const;		// Обход дерева в ширину
+	//void postorder(Node<T>* tree) const;		// Обход дерева в глубину
+
+	void clear(Node<T>*& root);								// Удаляет все узлы дерева и устанавливает указатель root в NULL. 
+	void deleteLeefs(T& data);								// Поиск листа для удаления
+	void deleteNode(Node<T>*& tree, Node<T>*& root);		// Удаляет лист
+	Node<T>* GetPredecessor(Node<T>* tree, Node<T>*& root);	// Находит "предыдущий" по значению лист
+	Node<T>* GetSuccessor(Node<T>* tree);					// Находит родителя элемента
+
+	void insert(Node<T>*& tree, T& data); // Добавляет лист
+public:
+	BinaryTree() : root(nullptr), count(0) {}; // Конструктор по умолчанию.
+
+	//const BinaryTree<T>& operator=(const BinaryTree<T>& otherTree); // Перегрузка оператора присваивания.
+	//BinaryTree(const BinaryTree<T>& otherTree); // Копирующий конструктор
+
+	ostream& operator<<(T& d)
+	{
+		return (cout << d);
+	}
+
+	bool isEmpty() const // Отвечает на вопрос содержит дерево элементы или нет.
+	{
+		if (root == nullptr) return 1;
+		return 0;
+	}
+
+	void print() const // Выводит данные из дерева.
+	{
+		inorderTree();
+	}
+
+	int nodeCount() const // Возвращает количество узлов в дереве.
+	{
+		return count;
+	}
+
+	void destroy() // Удаляет все узлы дерева. В результате дерево пусто и root == NULL;
+	{
+		clear(root);
+	}
+
+	void inorderTree() const // Симметричный обход дерева.
+	{
+		inorder(root);
+	}
+	int CountLeefs(T& item) const // Симметричный обход дерева с подсчётом его детей.
+	{
+		return inorderCountLeefs(retriveLeef(item));
+	}
+
+	//void preorderTree()const // Обход дерева в ширину.
+	//{
+	//	preorder(root);
+	//}
+
+	//void postorderTree()const // Обход дерева в глубину.
+	//{
+	//	postorder(root);
+	//}
+
+	T* retriveItem(T& item) const // Возвращает указатель на item, если найдено
+	{
+		return searchInfoLeef(root, item);
+	}
+
+	void deleteItem(T& item) // Удаляет узел с данными item из дерева.
+	{
+		deleteLeefs(root, item);
+	}
+
+	void insertItem(T& item) // Добавляет узел с данными item в дерево.
+	{
+		insert(root, item);
+	}
+
+	~BinaryTree() // Деструктор
+	{
+		destroy();
+	}
+};
+
+//template<class T> Node<T>* BinaryTree<T>::copyTree(BinaryTree<T>* otherTree) const
+//{
+//	if (otherTree->isEmpty()) return nullptr;
+//
+//	Node<T> ourTree = *this;
+//	ourTree.insertItem(otherTree->root->info);
+//	copyTree(otherTree->root->left);
+//	copyTree(otherTree->root->right);
+//	return ourTree;
+//}
+
+template<class T> void BinaryTree<T>::inorder(Node<T>* tree) const
+{
+	if (tree != NULL)
+	{
+		inorder(tree->left); //Рекурсивный вызов
+		cout << tree->info << " ";
+		//Или добавление в очередь или стэк...
+		inorder(tree->right); //Рекурсивный вызов
+	}
+}
+template<class T> int BinaryTree<T>::inorderCountLeefs(Node<T>* tree) const
+{
+	int cnt = 0;
+	if (tree != NULL)
+	{
+		++cnt;
+		cnt += inorderCountLeefs(tree->left);	//Рекурсивный вызов
+		cnt += inorderCountLeefs(tree->right);	//Рекурсивный вызов
+		return cnt;
+	}
+	else return 0;
+}
+
+template<class T> void BinaryTree<T>::clear(Node<T>*& root)
+{
+	while (!isEmpty()) // Пока дерево не пусто
+		deleteNode(root, root); // Удаляем элементы
+}
+
+template<class T> Node<T>* BinaryTree<T>::searchLeef(Node<T>* tree, T& data) const
+{
+	if (data < tree->info)				// Если данные «меньше», чем у текущего узла ...
+		searchLeef(tree->left, data);	// Идём налево
+	else if (data > tree->info)			// Если данные «больше», чем у текущего узла ...
+		searchLeef(tree->right, data);	// Идём направо
+	else if (data == tree->info) return tree; // Возвращаем указатель на объект
+}
+template<class T> T* BinaryTree<T>::searchInfoLeef(Node<T>* tree, T& data) const
+{
+	if (data < tree->info)				// Если данные «меньше», чем у текущеыго узла ...
+		searchLeef(tree->left, data);	// Идём налево
+	else if (data > tree->info)			// Если данные «больше», чем у текущего узла ...
+		searchLeef(tree->right, data);	// Идём направо
+	else if (data == tree->info) return new T(tree->info); // Возвращаем указатель на данные
+	// Не спрашивайте меня, зачем так делать, я сам не очень понимаю, скажите, если сами знаете
+}
+
+template<class T> void BinaryTree<T>::deleteLeefs(T& data)
+{
+	deleteNode(retriveLeef(data), root); // Удаляем объект, найденный по данным
+}
+
+template<class T> void BinaryTree<T>::deleteNode(Node<T>*& tree, Node<T>*& root)
+{
+	Node<T>* temp; Node<T>* temproot;
+
+	// Это пиздец, я просто промолчу (потратил свыше 5 часов на эту хуйню)
+
+	temp = tree, temproot = root;
+	if (tree->left == nullptr && tree->right != nullptr)		// Один ребёнок
+	{
+		temp = tree->right;										// Спускаемся вправо
+		tree->info = temp->info,								
+			tree->left = temp->left, tree->right = temp->right;	// Копируем (отбираем) там данные и детей
+		delete temp, --count;									// Удаляем, вычитаем
+		if (count == 0) tree = nullptr;							// Если 0, обнуляем указатель
+	}
+	else if (tree->right == nullptr && tree->left != nullptr)	// Один ребёнок
+	{
+		temp = tree->left;										// Спускаемся влево
+		tree->info = temp->info, 
+			tree->right = temp->right, tree->left = temp->left;	// Копируем (отбираем) там данные и детей
+		delete temp, --count;									// Удаляем, вычитаем
+		if (count == 0) tree = nullptr;							// Если 0, обнуляем указатель
+
+	}
+	else if (tree->right == nullptr && tree->left == nullptr)	// Если бездетный
+	{
+		temproot = GetSuccessor(temp);			// Находим предка	
+		if (temproot->right == temp) temproot->right = nullptr;	// Ну и обнуляем у предка 
+		else temproot->left = nullptr;							// указатель на ребёнка
+		delete temp, --count;									// Удаляем, вычитаем
+		if (count == 0) tree = nullptr;							// Если 0, обнуляем указатель
+	}
+	else														// Ровно два дочерних узла
+	{
+		Node<T>* pred = GetPredecessor(tree, temproot);	// Находит «предыдущий» элемент
+		int leefs = inorderCountLeefs(pred);						// Смотрим, сколько "листьев"
+		tree->info = pred->info;						// Записывает данные в «удаляемый» узел
+		if (leefs == 1)												// Если элемент бездетный, сразу удаляем, я так захотел, а то ебал я в рот >:(	
+		{
+			if (temproot->right == pred) temproot->right = nullptr;	// Ну и обнуляем у предка..
+			else temproot->left = nullptr;							// ..указатель на ребёнка
+			delete pred, --count;									// Удаляем, вычитаем
+			if (count == 0) tree = nullptr;							// Если 0, обнуляем указатель
+		}
+		else deleteNode(pred, temproot);				//Удаляет «предыдущий» элемент
+	}
+
+}
+
+template<class T> Node<T>* BinaryTree<T>::GetPredecessor(Node<T>* tree, Node<T>*& root)
+{
+	Node<T>* curr = tree->left;		// Спускаемся влево (можно вправо сделать, но тогда и while тоже так)
+	while (curr->right != nullptr)	// Идём направо, пока есть узлы...
+	{
+		root = curr;
+		curr = curr->right;
+	}
+	return curr; //Получаем указатель
+}
+template<class T> Node<T>* BinaryTree<T>::GetSuccessor(Node<T>* tree)
+{
+	// Идентично GetPredecessor в купе с searchLeef, только возвращаем предка, при находке элемента
+
+	Node<T>* curr = root;	// Начинаем с корня
+	Node<T>* currRoot;		// Спускаемся влево (можно вправо сделать, но тогда и while тоже так)
+	while (curr != tree)	// Идём направо, пока есть узлы...
+	{
+		currRoot = curr;
+		if (curr->info < tree->info)				// Если данные «меньше», чем у текущего узла ...
+			curr = tree->left;	// Идём налево
+		else if (curr->info > tree->info)			// Если данные «больше», чем у текущего узла ...
+			curr = tree->right;	// Идём направо
+		else if (curr->info == tree->info) return currRoot; // Возвращаем указатель на объект
+	}
+	return curr; //Получаем указатель
+}
+
+template<class T> void BinaryTree<T>::insert(Node<T>*& tree, T& data)
+{
+	{
+		if (tree == nullptr) // Основной случай
+			tree = new Node<T>(data, nullptr, nullptr), ++count;
+		else if (data < tree->info)
+			insert(tree->left, data);	// Рекурсия
+		else
+			insert(tree->right, data);	// Рекурсия
+	}
+}
+
+
+
+
+
+
+
+template<class T>
+class TreeCalс : public BinaryTree<T>
+{
+	using BinaryTree<T>::root;
+
+protected:
+	int num;
+public:
+	TreeCalс() : num(NULL) {};
+	~TreeCalс();
+
+
+
+
+};
