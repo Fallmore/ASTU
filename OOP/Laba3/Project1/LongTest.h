a0a801f359b4ad43c8a429fcc77abe523fa185bb
Fallmore <135610092+Fallmore@users.noreply.github.com>
2023-11-28 20:10:55 +0400

    Update LongTest.h

--- OOP/Laba3/Project1/LongTest.h
+++ OOP/Laba3/Project1/LongTest.h
@@ -4,12 +4,12 @@
 #include "Longtype.h"
 #include "UnitTest.h"
 
-using namespace UnitTest;         // -- ÷òîáû íå ïèñàòü ïîëíûå èìåíà --
-// -- â òåñòîâûõ ìåòîäàõ ïèøóòñÿ âûçîâû ìàêðîñà check() --
+using namespace UnitTest;         // -- чтобы не писать полные имена --
+// -- в тестовых методах пишутся вызовы макроса check() --
 class LongTest: public Test {
-    // -- çäåñü ìîæíî îïðåäåëÿòü îáúåêòû òåñòèðóåìîãî êëàññà --
+    // -- здесь можно определять объекты тестируемого класса --
     Long a; Long b; Long c;
-    Long d; Long e; Long f;    // -- îáúåêòû, íóæíû âñåì --
+    Long d; Long e; Long f;    // -- объекты, нужны всем --
     Long mas[6]; Long* dynMas = new Long[6];
   public:
       LongTest() : a(32313213l), b(static_cast<long>(INT32_MAX)), c(0l), d(b), e("234234234234234"), f("234234234234230") {
@@ -17,55 +17,55 @@ class LongTest: public Test {
         mas[3] = mas[1]; mas[4] = e; mas[5] = f;
         dynMas[0] = 32313213l; dynMas[1] = static_cast<long>(INT32_MAX);
         dynMas[3] = dynMas[1]; dynMas[4] = e; dynMas[5] = f;
-      } // -- èíèöèàëèçàöèÿ îáúåêòîâ --
-    // -- ìîæíî íàïèñàòü êîíñòðóêòîð äëÿ èíèöèàëèçàöèè îáåêòîâ-ïîëåé --
-    // - îïðåäåëåíèå ÷èñòîé âèðòóàëüíîé ôóíêöèè run() êëàññà Test --
-    void run() {                 // -- âûçîâ òåñòîâûõ ìåòîäâ --
-        testCreate();			// -- òåñò êîíñòðóêòîðîâ è ïðèñâàèâàíèÿ --
-        testArithmetic();	// -- òåñò àðèôìåòèêè --
-        testCompare();		// -- òåñò ñðàâíåíèÿ --
-        testCreateMas();			// -- òåñò êîíñòðóêòîðîâ è ïðèñâàèâàíèÿ --
-        testArithmeticMas();	// -- òåñò àðèôìåòèêè --
-        testCompareMas();		// -- òåñò ñðàâíåíèÿ --
-        testCreateDynMas();			// -- òåñò êîíñòðóêòîðîâ è ïðèñâàèâàíèÿ --
-        testArithmeticDynMas();	// -- òåñò àðèôìåòèêè --
-        testCompareDynMas();		// -- òåñò ñðàâíåíèÿ --
-        testExeption();		// -- òåñò èñêëþ÷åíèé --
+      } // -- инициализация объектов --
+    // -- можно написать конструктор для инициализации обектов-полей --
+    // - определение чистой виртуальной функции run() класса Test --
+    void run() {                 // -- вызов тестовых методв --
+        testCreate();			// -- тест конструкторов и присваивания --
+        testArithmetic();	// -- тест арифметики --
+        testCompare();		// -- тест сравнения --
+        testCreateMas();			// -- тест конструкторов и присваивания --
+        testArithmeticMas();	// -- тест арифметики --
+        testCompareMas();		// -- тест сравнения --
+        testCreateDynMas();			// -- тест конструкторов и присваивания --
+        testArithmeticDynMas();	// -- тест арифметики --
+        testCompareDynMas();		// -- тест сравнения --
+        testExeption();		// -- тест исключений --
     }
-    // -- òåñòîâûå ìåòîäû --
-    void testCreate() { 		// -- ñîçäàíèå è ïðèñâàèâàíèå --
+    // -- тестовые методы --
+    void testCreate() { 		// -- создание и присваивание --
         check(a == 32313213l);
         check(b == INT32_MAX);
         check(c == 0);
-        c = b;          check(c == a);    // -- îøèáêà - äîëæíî áûòü b --
+        c = b;          check(c == a);    // -- ошибка - должно быть b --
         check(d == b);
         check(e == f + 4);
         c = e;          check(c == f + 4);
-        c = 0l;   // -- âîçâðàùåíèå ê íà÷àëüíîìó çíà÷åíèþ --
+        c = 0l;   // -- возвращение к начальному значению --
     }
-    void testArithmetic() {     // -- òåñòèðîâàíèå ñëîæåíèÿ --
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Ñëåâà Long --
+    void testArithmetic() {     // -- тестирование сложения --
+        // -- арифметические операции. Слева Long --
         check(e == f + 4);
         check(e - 4 == f);
         check(e * 2 == f * 2 + 8);
         check(e / 2 == f / 2 + 2);
         check((e + 1) % 2 == 1);
         d = 1234;        check(d == 1234);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Ñïðàâà Long --
+        // -- арифметические операции. Справа Long --
         check(e == 4 + f);
         check(0 == 32313213 - a);
-        check(2 * a == 64626427);       // -- îøèáêà - äîëæíî áûòü 64626426 --
+        check(2 * a == 64626427);       // -- ошибка - должно быть 64626426 --
         check(64626426 / a == 2);
         check(14 % a == 14);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Îáà Long --
+        // -- арифметические операции. Оба Long --
         check(a + a == 64626426);
         check(0 == a - a);
         check(a * c == 0);
         check(a / a == 1);
         check(a % a == 0);
         d = f;        check(d == f);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. C ïðèñâàèâàíèåì --
-        c += f;             check(c == a);      // -- îøèáêà - äîëæíî áûòü f --
+        // -- арифметические операции. C присваиванием --
+        c += f;             check(c == a);      // -- ошибка - должно быть f --
         c += 4;            check(c == e);
         c -= 4;             check(c == f);
         c -= f - 1;        check(c == 1);
@@ -79,13 +79,13 @@ class LongTest: public Test {
         c++;                check(c == 3);
         --c;                   check(c == 2);
         c--;                   check(c == 1);
-        c = 0l;   // -- âîçâðàùåíèå ê íà÷àëüíîìó çíà÷åíèþ --
+        c = 0l;   // -- возвращение к начальному значению --
     }
-    void testCompare() {  		// -- ïðîâåðêà ñðàâíåíèÿ --
+    void testCompare() {  		// -- проверка сравнения --
         check(e < f == false);
         check(e > f == true);
         check(e <= f == false);
-        check(e >= f == false);     // -- îøèáêà - äîëæíî áûòü true 0 --
+        check(e >= f == false);     // -- ошибка - должно быть true 0 --
         check(e <= f + 4 == true);
         check(e >= f + 4 == true);
         check(e != f == true);
@@ -100,39 +100,39 @@ class LongTest: public Test {
         check(1 <= c == false);
         check(1 != c == true);
     }
-    void testCreateMas() { 		// -- ñîçäàíèå è ïðèñâàèâàíèå --
+    void testCreateMas() { 		// -- создание и присваивание --
         check(mas[0] == 32313213);
         check(mas[1] == INT32_MAX);
         check(mas[2] == 0);
-        mas[2] = mas[1];        check(mas[2] == mas[0]);  // -- îøèáêà - äîëæíî áûòü mas[1] --
+        mas[2] = mas[1];        check(mas[2] == mas[0]);  // -- ошибка - должно быть mas[1] --
         check(mas[3] == mas[1]);
         check(mas[4] == mas[5] + 4);
         mas[2] = mas[4];         check(mas[2] == mas[5] + 4);
-        mas[2] = 0l;   // -- âîçâðàùåíèå ê íà÷àëüíîìó çíà÷åíèþ --
+        mas[2] = 0l;   // -- возвращение к начальному значению --
     }
-    void testArithmeticMas() {     // -- òåñòèðîâàíèå ñëîæåíèÿ --
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Ñëåâà Long --
+    void testArithmeticMas() {     // -- тестирование сложения --
+        // -- арифметические операции. Слева Long --
         check(mas[4] == mas[5] + 4);
         check(mas[4] - 4 == mas[5]);
         check(mas[4] * 2 == mas[5] * 2 + 8);
         check(mas[4] / 2 == mas[5] / 2 + 2);
         check((mas[4] + 1) % 2 == 1);
         mas[3] = 1234; check(mas[3]== 1234);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Ñïðàâà Long --
+        // -- арифметические операции. Справа Long --
         check(mas[4] == 4 + mas[5]);
         check(0 == 32313213 - mas[0]);
-        check(2 * mas[0] == 64626427);       // -- îøèáêà - äîëæíî áûòü 64626426 --
+        check(2 * mas[0] == 64626427);       // -- ошибка - должно быть 64626426 --
         check(64626426 / mas[0] == 2);
         check(14 % mas[0] == 14);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Îáà Long --
+        // -- арифметические операции. Оба Long --
         check(mas[0] + mas[0] == 64626426);
         check(0 == mas[0] - mas[0]);
         check(mas[0] * mas[2] == 0);
         check(mas[0] / mas[0] == 1);
         check(mas[0] % mas[0] == 0);
         mas[3] = mas[5]; check(mas[3]== mas[5]);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. C ïðèñâàèâàíèåì --
-        mas[2] += mas[5]; check(mas[2] == mas[0]);      // -- îøèáêà - äîëæíî áûòü mas[5] --
+        // -- арифметические операции. C присваиванием --
+        mas[2] += mas[5]; check(mas[2] == mas[0]);      // -- ошибка - должно быть mas[5] --
         mas[2] += 4; check(mas[2] == mas[4]);
         mas[2] -= 4; check(mas[2] == mas[5]);
         mas[2] -= mas[5] - 1; check(mas[2] == 1);
@@ -146,9 +146,9 @@ class LongTest: public Test {
         mas[2]++; check(mas[2] == 3);
         --mas[2]; check(mas[2] == 2);
         mas[2]--; check(mas[2] == 1);
-        mas[2] = 0l;   // -- âîçâðàùåíèå ê íà÷àëüíîìó çíà÷åíèþ --
+        mas[2] = 0l;   // -- возвращение к начальному значению --
     }
-    void testCompareMas() {  		// -- ïðîâåðêà ñðàâíåíèÿ --
+    void testCompareMas() {  		// -- проверка сравнения --
         check(mas[4] < mas[5] == false);
         check(mas[4] > mas[5] == true);
         check(mas[4] <= mas[5] == false);
@@ -167,39 +167,39 @@ class LongTest: public Test {
         check(1 <= mas[2] == false);
         check(1 != mas[2] == true);
     }
-    void testCreateDynMas() { 		// -- ñîçäàíèå è ïðèñâàèâàíèå --
+    void testCreateDynMas() { 		// -- создание и присваивание --
         check(dynMas[0] == 32313213);
         check(dynMas[1] == INT32_MAX);
         check(dynMas[2] == 0);
-        dynMas[2] = dynMas[1];        check(dynMas[2] == dynMas[0]);  // -- îøèáêà - äîëæíî áûòü dynMas[1] --
+        dynMas[2] = dynMas[1];        check(dynMas[2] == dynMas[0]);  // -- ошибка - должно быть dynMas[1] --
         check(dynMas[3] == dynMas[1]);
         check(dynMas[4] == dynMas[5] + 4);
         dynMas[2] = dynMas[4];         check(dynMas[2] == dynMas[5] + 4);
-        dynMas[2] = 0l;   // -- âîçâðàùåíèå ê íà÷àëüíîìó çíà÷åíèþ --
+        dynMas[2] = 0l;   // -- возвращение к начальному значению --
     }
-    void testArithmeticDynMas() {     // -- òåñòèðîâàíèå ñëîæåíèÿ --
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Ñëåâà Long --
+    void testArithmeticDynMas() {     // -- тестирование сложения --
+        // -- арифметические операции. Слева Long --
         check(dynMas[4] == dynMas[5] + 4);
         check(dynMas[4] - 4 == dynMas[5]);
         check(dynMas[4] * 2 == dynMas[5] * 2 + 8);
         check(dynMas[4] / 2 == dynMas[5] / 2 + 2);
         check((dynMas[4] + 1) % 2 == 1);
         dynMas[3] = 1234; check(dynMas[3]== 1234);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Ñïðàâà Long --
+        // -- арифметические операции. Справа Long --
         check(dynMas[4] == 4 + dynMas[5]);
         check(0 == 32313213 - dynMas[0]);
-        check(2 * dynMas[0] == 64626427);       // -- îøèáêà - äîëæíî áûòü 64626426 --
+        check(2 * dynMas[0] == 64626427);       // -- ошибка - должно быть 64626426 --
         check(64626426 / dynMas[0] == 2);
         check(14 % dynMas[0] == 14);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. Îáà Long --
+        // -- арифметические операции. Оба Long --
         check(dynMas[0] + dynMas[0] == 64626426);
         check(0 == dynMas[0] - dynMas[0]);
         check(dynMas[0] * dynMas[2] == 0);
         check(dynMas[0] / dynMas[0] == 1);
         check(dynMas[0] % dynMas[0] == 0);
         dynMas[3] = dynMas[5]; check(dynMas[3]== dynMas[5]);
-        // -- àðèôìåòè÷åñêèå îïåðàöèè. C ïðèñâàèâàíèåì --
-        dynMas[2] += dynMas[5]; check(dynMas[2] == dynMas[0]);      // -- îøèáêà - äîëæíî áûòü dynMas[5] --
+        // -- арифметические операции. C присваиванием --
+        dynMas[2] += dynMas[5]; check(dynMas[2] == dynMas[0]);      // -- ошибка - должно быть dynMas[5] --
         dynMas[2] += 4; check(dynMas[2] == dynMas[4]);
         dynMas[2] -= 4; check(dynMas[2] == dynMas[5]);
         dynMas[2] -= dynMas[5] - 1; check(dynMas[2] == 1);
@@ -213,9 +213,9 @@ class LongTest: public Test {
         dynMas[2]++; check(dynMas[2] == 3);
         --dynMas[2]; check(dynMas[2] == 2);
         dynMas[2]--; check(dynMas[2] == 1);
-        dynMas[2] = 0l;   // -- âîçâðàùåíèå ê íà÷àëüíîìó çíà÷åíèþ --
+        dynMas[2] = 0l;   // -- возвращение к начальному значению --
     }
-    void testCompareDynMas() {  		// -- ïðîâåðêà ñðàâíåíèÿ --
+    void testCompareDynMas() {  		// -- проверка сравнения --
         check(dynMas[4] < dynMas[5] == false);
         check(dynMas[4] > dynMas[5] == true);
         check(dynMas[4] <= dynMas[5] == false);
@@ -235,12 +235,12 @@ class LongTest: public Test {
         check(1 != dynMas[2] == true);
     }
     void testExeption() {
-        try {                     // -- ýòî ðàáîòàåò ïðàâèëüíî
-            Long F('a');     // -- äîëæåí ãåíåðèòü èñêëþ÷åíèå --
-            fail();                 // -- ïðîâàëåííûé òåñò ïî èñêëþ÷åíèÿì --
-            error("Invalid Long undetected");     // -- áåç ïðåïðîöåññîðà íå îáîéòèñü --
+        try {                     // -- это работает правильно
+            Long F('a');     // -- должен генерить исключение --
+            fail();                 // -- проваленный тест по исключениям --
+            error("Invalid Long undetected");     // -- без препроцессора не обойтись --
         } catch(Long::LongError &) {
-            success();            // -- òîëüêî äëÿ ýòîãî ñäåëàí ìåòîä --
+            success();            // -- только для этого сделан метод --
         }
         return;
     };
